9b9917d19104ede25d5eabf45e2d0cc5f7da6b30
lbouvard <lbouvard57@gmail.com>
2015-07-23 05:04:29 -0700

    Test de bouton rond.

--- Calculatrice/Form1.Designer.cs
+++ Calculatrice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculatrice
 {
-    partial class Form1
+    partial class frmCalculatrice
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlTouche = new System.Windows.Forms.Panel();
+            this.btnShift = new System.Windows.Forms.Button();
+            this.pnlTouche.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 57);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlTouche
+            // 
+            this.pnlTouche.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTouche.Controls.Add(this.btnShift);
+            this.pnlTouche.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlTouche.Location = new System.Drawing.Point(0, 57);
+            this.pnlTouche.Name = "pnlTouche";
+            this.pnlTouche.Size = new System.Drawing.Size(360, 423);
+            this.pnlTouche.TabIndex = 1;
+            // 
+            // btnShift
+            // 
+            this.btnShift.Location = new System.Drawing.Point(3, 3);
+            this.btnShift.Name = "btnShift";
+            this.btnShift.Size = new System.Drawing.Size(68, 36);
+            this.btnShift.TabIndex = 0;
+            this.btnShift.Text = "Shift";
+            this.btnShift.UseVisualStyleBackColor = true;
+            // 
+            // frmCalculatrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(360, 480);
+            this.Controls.Add(this.pnlTouche);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCalculatrice";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculatrice";
+            this.pnlTouche.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlTouche;
+        private System.Windows.Forms.Button btnShift;
     }
 }
 
